7c40d17515696bd509806ecda14e29a7fd360d1f
sanyalh <sanyalh@yahoo.com>
2020-06-09 19:54:05 -0400

    Add files via upload

--- covid19.R
+++ covid19.R
@@ -8,6 +8,26 @@ View(covid19 %>% filter(administrative_area_level_1 == "India"))
 
 covid19 %>% group_by(administrative_area_level_1) %>% 
   summarize(deaths = sum(deaths)) %>% 
-  select(administrative_area_level_1, deaths) %>% 
-  ggplot(aes(administrative_area_level_1,deaths)) +
-  geom_bar(stat="identity")
+  #select(Country, deaths) %>% 
+  arrange(desc(deaths)) %>% 
+  head(20) %>%
+  mutate(Country = fct_reorder(administrative_area_level_1,deaths)) %>%
+  ggplot(aes(Country,deaths, fill=Country)) +
+  geom_bar(stat="identity") +
+  labs(x="Country", 
+       y="Deaths", 
+       title="Coronavirus deaths per Country",
+       fill="Country")
+
+covid19 %>% group_by(administrative_area_level_1) %>% 
+  summarize(confirmed = sum(confirmed)) %>% 
+  #select(Country, deaths) %>% 
+  arrange(desc(confirmed)) %>% 
+  head(20) %>%
+  mutate(Country = fct_reorder(administrative_area_level_1,confirmed)) %>%
+  ggplot(aes(Country,confirmed, fill=Country)) +
+  geom_bar(stat="identity") +
+  labs(x="Country", 
+       y="Confirmed Cases", 
+       title="Coronavirus confirmed cases per Country",
+       fill="Country")
