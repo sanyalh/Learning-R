a26bb382d3d21d5b5a3f3a3e55c5afb42b49c9a6
sanyalh <sanyalh@yahoo.com>
2020-06-10 18:16:05 -0400

    Add files via upload

--- covid19.R
+++ covid19.R
@@ -1,19 +1,33 @@
 #install.packages("COVID19")
+library(tidyverse)
 library(COVID19)
 
 covid19 <- covid19()
-View(covid19)
-covid19 %>% select(starts_with("administrative_area_level"))
-View(covid19 %>% filter(administrative_area_level_1 == "India"))
+
+covid19 %>% filter(administrative_area_level_1 == "India")
+
+# covid19 %>% count(administrative_area_level_1, wt=deaths, sort=TRUE) %>% 
+#   head(20) %>%
+#   mutate(Country = fct_reorder(administrative_area_level_1,n)) %>%
+#   ggplot(aes(Country,n, fill=Country)) +
+#   geom_bar(stat="identity") +
+#   scale_y_continuous(labels = comma_format()) +
+#   coord_flip() +
+#   labs(x="Country", 
+#        y="Deaths", 
+#        title="Coronavirus deaths per Country",
+#        fill="Country")
+
 
 covid19 %>% group_by(administrative_area_level_1) %>% 
   summarize(deaths = sum(deaths)) %>% 
-  #select(Country, deaths) %>% 
   arrange(desc(deaths)) %>% 
   head(20) %>%
   mutate(Country = fct_reorder(administrative_area_level_1,deaths)) %>%
   ggplot(aes(Country,deaths, fill=Country)) +
   geom_bar(stat="identity") +
+  scale_y_continuous(labels = scales::label_comma()) +
+  coord_flip() +
   labs(x="Country", 
        y="Deaths", 
        title="Coronavirus deaths per Country",
@@ -21,12 +35,13 @@ covid19 %>% group_by(administrative_area_level_1) %>%
 
 covid19 %>% group_by(administrative_area_level_1) %>% 
   summarize(confirmed = sum(confirmed)) %>% 
-  #select(Country, deaths) %>% 
   arrange(desc(confirmed)) %>% 
   head(20) %>%
   mutate(Country = fct_reorder(administrative_area_level_1,confirmed)) %>%
   ggplot(aes(Country,confirmed, fill=Country)) +
   geom_bar(stat="identity") +
+  scale_y_continuous(labels = scales::label_comma()) +
+  coord_flip() +
   labs(x="Country", 
        y="Confirmed Cases", 
        title="Coronavirus confirmed cases per Country",
